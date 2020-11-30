2bcbe238d1fc71a33a616ae30050d2f32356f6b5
admin <makoto.miyazaki@dataiku.com>
2020-11-30 14:48:20 +0100

    Saved web app test (FRAUD_DETECTION.MVdoeRM)

--- web_apps/MVdoeRM/backend.py
+++ web_apps/MVdoeRM/backend.py
@@ -19,10 +19,7 @@ sns.heatmap(uniform_data)
 
 
 # Set up data
-N = 200
-x = np.linspace(0, 4*np.pi, N)
-y = np.sin(x)
-source = ColumnDataSource(data=dict(x=x, y=y))
+
 
 
 # Set up plot
@@ -30,11 +27,9 @@ plot = figure(plot_height=400, plot_width=400, title="my sine wave",
               tools="crosshair,pan,reset,save,wheel_zoom",
               x_range=[0, 4*np.pi], y_range=[-2.5, 2.5])
 
-plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6)
-
 
 
 # Set up layouts and add to document
-curdoc().add_root(row(plt, width=800))
+curdoc().add_root(row(fig, width=800))
 
 
